f0ce5b74836c00251b08bf15a548a13e3fd8476d
Danilo Filitto <danilo.filitto@gmail.com>
2019-04-17 19:20:25 -0300

    Versão 4 - com o arquivo que ira executar op crud no banco

---
+++ AppFinanceiro/AppFinanceiro/AppFinanceiro/Controller/DBFinanca.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+using AppFinanceiro.Models;
+
+namespace AppFinanceiro.Controller
+{
+    public class DBFinanca
+    {
+        SQLiteConnection conn; //representa o banco de dados
+        public string StatusMessage { get; set; } //representa a mensagem da ultima operação
+
+        public DBFinanca(string dbPath)
+        {
+            conn = new SQLiteConnection(dbPath);
+            conn.CreateTable<Financa>(); //cria a tabela no banco se ela não existir
+        }
+
+        public void Inserir(Financa financa)
+        {
+
+        }
+
+        public void Alterar(Financa financa)
+        {
+
+        }
+
+        public void Excluir(int Id)
+        {
+
+        }
+
+        //localizar
+        //mais um montão de coisas
+    }
+}
---
+++ AppFinanceiro/AppFinanceiro/AppFinanceiro/Models/Financa.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace AppFinanceiro.Models
+{
+    [Table("Financas")]
+    public class Financa
+    {
+        
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [MaxLength(250), Unique]
+        public string Descricao { get; set; }
+
+        [MaxLength(250), Unique]
+        public string TipoOperacao { get; set; } 
+
+        public DateTime Data { get; set; }
+        public Double valor { get; set; }
+    }
+}
